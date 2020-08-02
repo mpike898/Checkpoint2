df0aa3178480d05b4e85043480a33f6cfd6314d2
Matt Pike <mpike_88@hotmail.com>
2020-08-02 15:04:21 -0600

    first commit

---
+++ Tests/test.js
@@ -0,0 +1,60 @@
+var coolPage = {}
+
+
+module.exports = {
+    beforeEach: browser => {
+        coolPage = browser.page.coolPage()
+        coolPage.navigate()
+    },
+    after: browser=> {
+        coolPage.end()
+    },
+    'User can sort odd and even numbers': browser => {
+        coolPage
+        .waitForElementVisible('@evenOddInput')
+        .setValue('@evenOddInput', '1,2,3,4,5,6')
+        .click('@evenOddButton')
+        .verify.containsText('@evenResults','2,4,6')
+        .verify.containsText('@oddResults','1,3,5')
+
+
+    },
+    'User can filter objects by their properties in the filter object field': browser => {
+        coolPage
+        .waitForElementVisible('@objectFilterObjects')
+        .setValue('@objectFilterInput', 'hairColor')
+        .click('@objectFilterButton')
+        .verify.containsText('@objectFilterResults','brown')
+
+    },
+    'User can filter strings by their own partial string': browser => {
+        coolPage
+        .waitForElementVisible('@nameFilterInput')
+        .setValue('@nameFilterInput', 'Ma')
+        .click('@nameFilterButton')
+        .verify.containsText('@nameFilterResults','Mark')
+        .verify.containsText('@nameFilterResults','Maddy')
+
+    },
+    'User can enter text into the palindrome field and see if its a palindrome or not': browser => {
+        coolPage
+        .waitForElementVisible('@palindromeInput')
+        .setValue('@palindromeInput', 'tacocat')
+        .click('@palindromeButton')
+        .verify.containsText('@palindromeResults','true')
+
+
+    },
+    'User can input two numbers into the sum field and it will add them together': browser => {
+        coolPage
+        .waitForElementVisible('@sumInput1')
+        .setValue('@sumInput1', '2')
+        .setValue('@sumInput2', '2')
+        .click('@sumButton')
+        .verify.containsText('@sumResults','4')
+
+
+
+    },
+}
+
---
+++ pageObjects/coolPage.js
@@ -0,0 +1,87 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+    
+    
+
+    evenOddInput: {
+        selector: '[name="evenOddInput"]',
+    },
+  
+    evenOddButton: {
+        selector: '[name="evenOddButton"]',
+    },
+
+    evenResults: {
+        selector: '[name="evenResults"]',
+    },
+
+    oddResults: {
+        selector: '[name="oddResults"]',
+    },
+
+    objectFilterObjects: {
+        selector: '[name="objectFilterObjects"]',
+    },
+
+    objectFilterInput: {
+        selector: '[name="objectFilterInput"]',
+    },
+
+    objectFilterButton: {
+        selector: '[name="objectFilterButton"]',
+    },
+
+    objectFilterResults: {
+        selector: '[name="objectFilterResults"]',
+    },
+
+    nameFilterNames: {
+        selector: '[name="nameFilterNames"]',
+    },
+
+    nameFilterInput: {
+        selector: '#nameFilterInput',
+    },
+
+    nameFilterButton: {
+        selector: '#nameFilterButton',
+    },
+
+    nameFilterResults: {
+        selector: '[name="nameFilterResults"]',
+    },
+
+    palindromeInput: {
+        selector: '[name="palindromeInput"]',
+    },
+
+    palindromeButton: {
+        selector: '[name="palindromeButton"]',
+    },
+
+    palindromeResults: {
+        selector: '[name="palindromeResults"]',
+    },
+
+    sumInput1: {
+        selector: '[name="sumInput1"]',
+    },
+
+    sumInput2: {
+        selector: '[name="sumInput2"]',
+    },
+
+    sumButton: {
+        selector: '[name="sumButton"]',
+    },
+
+    sumResults: {
+        selector: '[name="sumResults"]',
+    },
+
+        }
+
+    
+    
+    }//*[contains(@class,"jSqgxr")]
